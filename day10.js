31cb5a1d69a4b8858a80a278a13003bf52e0340f
Łukasz Lalik <lukasz.lalik@bolt.eu>
2023-12-17 16:34:10 +0000

    day 10 part 2

--- day10.js
+++ day10.js
@@ -16,7 +16,7 @@ function part1(input) {
 
   let longest = 0;
   for (const startPosition of startPositions) {
-    const len = loopLength(sPos, startPosition);
+    const [len, path] = loopLength(sPos, startPosition);
     if (len > longest) longest = len;
   }
 
@@ -33,26 +33,89 @@ function getS() {
 
 function loopLength(previous, current) {
   let length = 0;
+  const path = [previous];
 
   while (true) {
+    path.push(current);
     const currentValue = getValue(current);
 
-    if (currentValue === '.') return -1;
+    if (currentValue === '.') return [-1, path];
 
     length++;
     if (currentValue === 'S') break;
 
     const next = getNext(previous, current);
-    if (!next) return -1;
+    if (!next) return [-1, path];
 
     previous = current;
     current = next;
   }
 
-  return length;
+  return [length, path];
 }
 
 function part2(input) {
+  ground = input.split('\n');
+  for (let i = 0; i < ground.length; i++) {
+    ground[i] = ground[i].split('');
+  }
+
+  const sPos = getS();
+  const startPositions = [
+    [sPos[0]-1, sPos[1]-1],
+    [sPos[0]-1, sPos[1]],
+    [sPos[0]-1, sPos[1]+1],
+    [sPos[0], sPos[1]-1],
+    [sPos[0], sPos[1]+1],
+    [sPos[0]+1, sPos[1]-1],
+    [sPos[0]+1, sPos[1]],
+    [sPos[0]+1, sPos[1]+1],
+  ];
+
+  let longest = 0;
+  let longestPath;
+  for (const startPosition of startPositions) {
+    const [len, path] = loopLength(sPos, startPosition);
+    if (len > longest) {
+      longest = len;
+      longestPath = path;
+    }
+  }
+
+  ground[sPos[0]][sPos[1]] = segmentForS(longestPath[longestPath.length - 2], longestPath[1]);
+
+  const polygon = {};
+  for (const coordinate of longestPath) {
+    const value = getValue(coordinate); 
+    if (['|', 'L', 'J', 'F', '7'].includes(value)) {
+      polygon[`${coordinate[0]}-${coordinate[1]}`] = value;   
+    }
+  }
+
+  let inside = 0;
+  for (let row = 0; row < ground.length; row++) {
+    let crosses = 0;
+    for (let col = 0; col < ground[row].length; col++) {
+      const coord = `${row}-${col}`;
+      let value = getValue([row, col]);
+      
+      if (coord in polygon && ['F', 'L'].includes(value)) {
+        const start = value;
+        do {
+          col++
+          value = getValue([row, col]);
+        } while (!['7', 'J'].includes(value))
+        if (start === 'F' && value === 'J') crosses++;
+        if (start === 'L' && value === '7') crosses++;
+      } else if (coord in polygon) {
+        crosses++;
+      } else if (crosses % 2 !== 0) {
+        inside++;
+      }
+    }
+  }
+
+  return inside;
 }
 
 function getValue([row, col]) {
@@ -60,6 +123,15 @@ function getValue([row, col]) {
   if (row === ground.length || col === ground[0].length) return '.';
 
   return ground[row][col];
+}
+
+function segmentForS([beforeRow, beforeCol], [afterRow, afterCol]) {
+  if (beforeCol === afterCol) return '|';
+  if (beforeRow === afterRow) return '-';
+  if (afterRow > beforeRow && afterCol > beforeCol) return 'L';
+  if (afterRow > beforeRow && afterCol < beforeCol) return 'J';
+  if (afterRow < beforeRow && afterCol > beforeCol) return 'F';
+  if (afterRow < beforeRow && afterCol < beforeCol) return '7';
 }
 
 function getNext(previous, current) {
--- day10.test.js
+++ day10.test.js
@@ -7,10 +7,22 @@ SJ.L7
 |F--J
 LJ...`;
 
+  const input2 = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`;
+
   test('part1', () => {
     expect(part1(input)).toBe(8);
   });
 
   test('part2', () => {
+    expect(part2(input2)).toBe(8);
   });
 });
