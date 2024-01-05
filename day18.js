a377552a8a1672652bdfbfe3c06ee575011f0116
Łukasz Lalik <lukasz.lalik@bolt.eu>
2024-01-05 13:12:26 +0100

    day18

---
+++ day18.js
@@ -0,0 +1,191 @@
+function part1(input) {
+  const parsed = parse(input);
+  const [trench, boundaries] = build(parsed);
+
+  return getVolume(trench, boundaries);
+}
+
+function part2(input) {
+  const parsed = parse2(input);
+  const [trench, boundaries] = build(parsed);
+  console.log('built');
+
+  return getVolume(trench, boundaries);
+}
+
+function parse(input) {
+  const lines = input.split('\n');
+
+  return lines
+    .filter(line => line.length > 0)
+    .map((line) => {
+      const parts = line.split(' ');
+      return [
+        parts[0],
+        parseInt(parts[1]),
+        parts[2].substr(2, parts[2].length - 3)
+      ]
+    });
+}
+
+function parse2(input) {
+  const lines = input.split('\n');
+
+  return lines
+    .filter(line => line.length > 0)
+    .map((line) => {
+      const parts = line.split(' ');
+      const code = parts[2].substr(2, parts[2].length - 3);
+
+      return parseCode(code);
+    });
+}
+
+function parseCode(code) {
+  const directionMap = ['R', 'D', 'L', 'U'];
+  const dist = parseInt(code.substr(0, code.length-1), 16);
+  const direction = directionMap[parseInt(code.substr(code.length-1, 1))];
+
+  return [direction, dist];
+}
+
+function build(commands) {
+  const result = {};
+  let maxCol = Number.MIN_SAFE_INTEGER;
+  let maxRow = Number.MIN_SAFE_INTEGER;
+  let minCol = Number.MAX_SAFE_INTEGER;
+  let minRow = Number.MAX_SAFE_INTEGER;
+
+  let current = [0, 0, commands[0][0]];
+  for (const command of commands) {
+    for (let i = 0; i < command[1]; i++) {
+      const [row, col] = current;
+      if (row > maxRow) maxRow = row;
+      if (row < minRow) minRow = row;
+      if (col > maxCol) maxCol = col;
+      if (col < minCol) minCol = col;
+
+      const next = move(current, command[0]);
+      const key = `${current[0]},${current[1]}`;
+      result[key] = getSegment(current[2], next[2]);
+
+      current = next;
+    }
+
+    const [row, col] = current;
+    if (row > maxRow) maxRow = row;
+    if (row < minRow) minRow = row;
+    if (col > maxCol) maxCol = col;
+    if (col < minCol) minCol = col;
+  }
+
+  result['0,0'] = getSegment(commands[commands.length-1][0], commands[0][0]);
+
+  return [result, [minRow, maxRow, minCol, maxCol]];
+}
+
+function move([row,col], direction) {
+  switch (direction) {
+    case 'U':
+      return [row-1, col, direction];
+    case 'D':
+      return [row+1, col, direction];
+    case 'L':
+      return [row, col-1, direction];
+    case 'R':
+      return [row, col+1, direction];
+  }
+}
+
+function getSegment(prev, curr) {
+  switch (prev) {
+    case 'U':
+      if (curr === 'U') return '|';
+      if (curr === 'L') return '7';
+      if (curr === 'R') return 'F';
+    case 'D':
+      if (curr === 'D') return '|';
+      if (curr === 'L') return 'J';
+      if (curr === 'R') return 'L';
+    case 'L':
+      if (curr === 'L') return '-';
+      if (curr === 'U') return 'L';
+      if (curr === 'D') return 'F';
+    case 'R':
+      if (curr === 'R') return '-';
+      if (curr === 'U') return 'J';
+      if (curr === 'D') return '7';
+  }
+
+  if (prev === 'U' && curr === 'U') {
+    return '|';
+  }
+  if (prev === 'D' && curr === 'D') {
+    return '|';
+  }
+  if (prev === 'L' && curr === 'L') {
+    return '-';
+  }
+  if (prev === 'R' && curr === 'R') {
+    return '-';
+  }
+}
+
+function getVolume(trench, boundaries) {
+  const [minRow, maxRow, minCol, maxCol] = boundaries;
+  let volume = 0;
+
+  for (let row = minRow; row <= maxRow; row++) {
+    let previous = [0, 0, undefined];
+    let inside = false;
+    for (let col = minCol; col <= maxCol; col++) {
+      const key = `${row},${col}`;
+      switch (trench[key]) {
+        case '-':
+          volume++;
+          break;
+        case '|':
+          volume++;
+          if (inside) {
+            volume += (col - previous[1]) - 1;
+          }
+          inside = !inside;
+          previous = [row, col, trench[key]];
+          break;
+        case 'L':
+          volume++;
+          if (inside) {
+            volume += (col - previous[1]) - 1;
+          }
+          previous = [row, col, trench[key]];
+          break;
+        case 'F':
+          volume++;
+          if (inside) {
+            volume += (col - previous[1]) - 1;
+          }
+          previous = [row, col, trench[key]];
+          break;
+        case 'J':
+          if (previous[2] === 'F') {
+            inside = !inside;
+          }
+          volume++;
+          previous = [row, col, trench[key]];
+          break;
+        case '7':
+          if (previous[2] === 'L') {
+            inside = !inside;
+          }
+          volume++;
+          previous = [row, col, trench[key]];
+          break;
+      }
+      
+    }
+  }
+
+  return volume;
+}
+
+module.exports = {part1, part2};
---
+++ day18.test.js
@@ -0,0 +1,27 @@
+const {part1, part2, MinPQ} = require('./day18');
+
+describe('day18', () => {
+  const input = `R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+`;
+
+  test('part1', () => {
+    expect(part1(input)).toBe(62);
+  });
+
+  test('part2', () => {
+    expect(part2(input)).toBeUndefined();
+  });
+});
